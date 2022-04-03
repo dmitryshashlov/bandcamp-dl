4309bc69ded64006a182b89d3521b4e0856cf1ae
Dmitry Shashlov <shashlov@gmail.com>
2022-04-03 14:03:20 +0200

    print album label

--- bandcamp_dl/__main__.py
+++ bandcamp_dl/__main__.py
@@ -115,7 +115,7 @@ def main():
     if arguments['--print-title']:
         for album in albumList:
             for track in album['tracks']:
-                print("%s|%s|%s|%s" % (track['purchase_url'], album['title'], track['title'], album['artist']))
+                print("%s|%s|%s|%s|%s" % (track['purchase_url'], album['title'], track['title'], album['artist'], album['label']))
     elif arguments['URL'] or arguments['--artist']:
         logging.debug("Preparing download process..")
         for album in albumList:
